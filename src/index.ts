4e38e14ae4e780e772da3862d7b996e719ecf7a1
jenetic <jznnylam@gmail.com>
2022-07-26 23:17:37 -0700

    Parse JSON

--- src/index.ts
+++ src/index.ts
@@ -6,13 +6,17 @@ const submitFile = () => {
 
     reader.readAsText(file, "UTF-8");
     reader.onload = (evt) => {
-      document.getElementById("file-contents").innerHTML = evt.target.result as string;
+      const contentString: string = evt.target.result as string;
+      const content = JSON.parse(contentString);
+      console.log(content);
+
+      document.getElementById("file-contents").innerHTML = content.participants[1].name as string;
+    }
+    reader.onerror = (evt) => {
+      document.getElementById("file-contents").innerHTML = "error reading file";
     }
   }
 }
 
-const hi = () => {
-  console.log("hi");
-}
-
+// Webpack does not make functions global so HTML onclick doesn't work?
 document.getElementById('submit-button').onclick = function() {submitFile()};
