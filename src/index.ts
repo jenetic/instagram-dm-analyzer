5d512867c5be207682a42ba80e90b1efb2711a77
jenetic <jznnylam@gmail.com>
2022-07-27 08:14:03 -0700

    Enable multiple file upload and print out list of files to console

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,5 @@
+import './main.css';
+
 const submitFile = () => {
   console.log("hi");
   const file = (<HTMLInputElement>document.getElementById("file-input")).files[0];
@@ -19,4 +21,43 @@ const submitFile = () => {
 }
 
 // Webpack does not make functions global so HTML onclick doesn't work?
-document.getElementById('submit-button').onclick = function() {submitFile()};
+// document.getElementById('submit-button').onclick = function() {submitFile()};
+
+const dropArea = document.getElementById('drop-area');
+
+function traverseFileTree(item: any, path="") {
+  // path = path || "";
+  if (item.isFile) {
+    // Get file
+    item.file(function(file: File) {
+      console.log("File:", path + file.name);
+    });
+  } else if (item.isDirectory) {
+    // Get folder contents
+    var dirReader = item.createReader();
+    dirReader.readEntries(function(entries: any) {
+      for (var i=0; i<entries.length; i++) {
+        traverseFileTree(entries[i], path + item.name + "/");
+      }
+    });
+  }
+}
+
+const submitButton = document.getElementById('submit-button');
+
+submitButton.addEventListener("click", function(event) {
+  event.preventDefault();
+  console.log("hi");
+
+  const items: any = (<HTMLInputElement>document.getElementById("file-input")).files;
+
+  console.log("items:", items);
+  // // var items = event.dataTransfer.items;
+  // for (var i=0; i<items.length; i++) {
+  //   // webkitGetAsEntry is where the magic happens
+  //   var item = items[i].webkitGetAsEntry();
+  //   if (item) {
+  //     traverseFileTree(item);
+  //   }
+  // }
+}, false);
