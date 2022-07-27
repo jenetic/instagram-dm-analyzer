f46748afb9ffd0fcfb44297a5787bc40140b6059
jenetic <jznnylam@gmail.com>
2022-07-27 15:57:24 -0700

    Enable user to toggle between 2 diff things to view when clicking on chat

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,8 @@
 import './main.css';
 
+let files: File[] = [];
+// let dropDownSelection: string = "";
+
 // Turns file inputs into array and removes non-JSON files
 const processFileInput = (fileList: FileList): File[] => {
   let files: File[] = Array.from(fileList);
@@ -9,38 +12,57 @@ const processFileInput = (fileList: FileList): File[] => {
   return files;
 }
 
+const getFirstParticipant = (content: any) => {
+  return content.participants[0].name;
+}
+
 // Main
 document.getElementById('submit-button').addEventListener("click", function(event) {
   event.preventDefault();
-  const files: File[] = processFileInput((<HTMLInputElement>document.getElementById("file-input")).files);
+  files = processFileInput((<HTMLInputElement>document.getElementById("file-input")).files);
 
   files.forEach(file => {
-    const filesList = document.getElementById("dm-list")
-    const divElement = document.createElement("div");
+    const filesList = document.getElementById("dm-list");
+    const buttonHeader = document.getElementById("main-header");
+    const mainContent = document.getElementById("main-content");
     
     const reader = new FileReader();
     reader.readAsText(file, "UTF-8");
     reader.onload = (evt) => {
       const contentString: string = evt.target.result as string;
       const content = JSON.parse(contentString);
+      
+      const sideButton = document.createElement("button");
+      sideButton.setAttribute("class", "side-button");
+      sideButton.textContent = content.title;
+      sideButton.onclick = () => {
 
-      // const p = document.createElement("p");
-      const main = document.getElementById("main");
-      const button = document.createElement("button");
-      button.textContent = content.title;
-      button.onclick = () => {
-        main.textContent = JSON.stringify(content);
+        // Sub buttons
+        const firstPartipicantBtn = document.createElement("button");
+        firstPartipicantBtn.textContent = "First Participant";
+        firstPartipicantBtn.onclick = () => {
+          mainContent.innerHTML = "";
+          mainContent.textContent = getFirstParticipant(content);
+        };
+
+        const contentBtn = document.createElement("button");
+        contentBtn.textContent = "Content";
+        contentBtn.onclick = () => {
+          mainContent.innerHTML = "";
+          mainContent.textContent = JSON.stringify(content);
+        };
+
+        buttonHeader.innerHTML = "";
+        mainContent.innerHTML = "";
+        buttonHeader.appendChild(firstPartipicantBtn);
+        buttonHeader.appendChild(contentBtn); 
       }
-      divElement.appendChild(button);
-      filesList.appendChild(divElement);
+      filesList.appendChild(sideButton);
     }
-    
-
     reader.onerror = (evt) => {
       document.getElementById("file-contents").innerHTML = "error reading file";
     }
   })
-
   document.getElementById("no-files-message").style.display = "none";
 }, false);
 
