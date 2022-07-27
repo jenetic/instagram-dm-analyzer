ed8536999cfef724183c6f22b5f401dc13a9b9bd
jenetic <jznnylam@gmail.com>
2022-07-27 09:12:09 -0700

    Basic parsing of message contents and displaying them onto page

--- src/index.ts
+++ src/index.ts
@@ -1,63 +1,47 @@
 import './main.css';
 
-const submitFile = () => {
-  console.log("hi");
-  const file = (<HTMLInputElement>document.getElementById("file-input")).files[0];
-  if (file) {
-    const reader = new FileReader();
+// Turns file inputs into array and removes non-JSON files
+const processFileInput = (fileList: FileList): File[] => {
+  let files: File[] = Array.from(fileList);
+  files = files.filter(file => {
+    return file.name.substring(file.name.length - 4) === "json";
+  })
+  return files;
+}
 
+// Main
+document.getElementById('submit-button').addEventListener("click", function(event) {
+  event.preventDefault();
+  const files: File[] = processFileInput((<HTMLInputElement>document.getElementById("file-input")).files);
+
+  files.forEach(file => {
+    const filesList = document.getElementById("files")
+    const divElement = document.createElement("div");
+    
+    const reader = new FileReader();
     reader.readAsText(file, "UTF-8");
     reader.onload = (evt) => {
       const contentString: string = evt.target.result as string;
       const content = JSON.parse(contentString);
-      console.log(content);
 
-      document.getElementById("file-contents").innerHTML = content.participants[1].name as string;
+      const p = document.createElement("p");
+      p.textContent = content.participants[0].name;
+      
+      const button = document.createElement("button");
+      button.textContent = content.participants[0].name;
+      button.onclick = () => {
+        console.log(content.participants[0].name);
+      }
+
+      divElement.appendChild(p);
+      divElement.appendChild(button);
+      filesList.appendChild(divElement);
     }
+    
+
     reader.onerror = (evt) => {
       document.getElementById("file-contents").innerHTML = "error reading file";
     }
-  }
-}
-
-// Webpack does not make functions global so HTML onclick doesn't work?
-// document.getElementById('submit-button').onclick = function() {submitFile()};
-
-const dropArea = document.getElementById('drop-area');
-
-function traverseFileTree(item: any, path="") {
-  // path = path || "";
-  if (item.isFile) {
-    // Get file
-    item.file(function(file: File) {
-      console.log("File:", path + file.name);
-    });
-  } else if (item.isDirectory) {
-    // Get folder contents
-    var dirReader = item.createReader();
-    dirReader.readEntries(function(entries: any) {
-      for (var i=0; i<entries.length; i++) {
-        traverseFileTree(entries[i], path + item.name + "/");
-      }
-    });
-  }
-}
-
-const submitButton = document.getElementById('submit-button');
-
-submitButton.addEventListener("click", function(event) {
-  event.preventDefault();
-  console.log("hi");
-
-  const items: any = (<HTMLInputElement>document.getElementById("file-input")).files;
-
-  console.log("items:", items);
-  // // var items = event.dataTransfer.items;
-  // for (var i=0; i<items.length; i++) {
-  //   // webkitGetAsEntry is where the magic happens
-  //   var item = items[i].webkitGetAsEntry();
-  //   if (item) {
-  //     traverseFileTree(item);
-  //   }
-  // }
+  })
 }, false);
+
